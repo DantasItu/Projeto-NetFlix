77b7877a830f91179d1b6f138e7d06b5673be46f
unknown <michael.of.douglasaraujodantas5@gmail.com>
2025-03-17 12:25:35 -0300

    criação das rotas CRUD do ws

--- netflix-ws/index.js
+++ netflix-ws/index.js
@@ -1,13 +1,41 @@
 const express = require('express');
+const morgan = require ('morgan');
 const app = express();
 
-app.get('/', (req,res)=>{
-// regras de negócio
-res.json({mensagem:'Rota Funcionando'});
+app.use(morgan('dev'));
 
+// regras de negócio
+// RECUPERAR TODOS OS REGISTROS
+app.get('/', (req,res)=>{
+res.json({mensagem:'PEGAR TODOS OS REGISTROS'});
 });
+
+
+// RECUPERAR SOMENTE O  REGISTRO COM O ID ==
+app.get('/:id',(req,res)=> {
+    const id = req.params.id;
+    res.json({mensagem:`pegar somente o registro com o id:${id}`})
+});
+
+// CRIAR UM REGISTRO
+app.post('/',(req,res)=>{
+    const body = req.body;
+    res.json({mensagem:"Criar usuario"});
+});
+
+// ATUALIZAR SOMENTE O REGISTRO COM ID ==
+app.put('/:id',(req,res)=>{
+    const id = req.params.id;
+res.json({mensagem: `ATUALIZAR SOMENTO O REGISTRO COM O ID: ${id}`})
+});
+
+
+// DELETAR SOMENTE O REGISTRO COM ID ==
+app.delete('/:id',(req,res)=>{
+    const id = req.params.id;
+    res.json({mensagem: `DELETAR SOMENTO O REGISTRO COM O ID: ${id}`})
+})
 
 app.listen(5000, () =>{
     console.log('meu servidor está funcionando');
-})
-// test3
+});
